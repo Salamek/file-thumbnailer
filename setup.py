bc27c8b82a21387f4bad4c5a9b1acb5cf636e8ef
Adam Schubert <adam.schubert@sg1-game.net>
2021-11-05 02:26:43 +0100

    New version 0.0.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_readme() -> str:
 
 setup(
     name='file-thumbnailer',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(exclude=['tests', 'tests.*']),
     package_data={'file_thumbnailer': ['py.typed']},
     install_requires=[
